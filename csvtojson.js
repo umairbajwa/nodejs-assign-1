a5cc7d2f239e483181d065a3712bc1eb77050ee0
Umair Nisar Bajwa <bajwa@devstudio.us>
2018-10-26 14:32:55 +0500

    adding csv + json

--- csvtojson.js
+++ csvtojson.js
@@ -1,10 +1,10 @@
 const csv = require('csvtojson')
-const csvfile = 'customer-data.csv'
+const csvfile = 'data.csv'
 const fs = require('fs')
 var bajwa;
 
 function callback() {};
 var json =csv().fromFile(csvfile).then((jsonObj)=>{
 bajwa = JSON.stringify(jsonObj)
-    fs.writeFileSync('out.json',bajwa) }
+    fs.writeFileSync('data.json',bajwa) }
 )
